5e7fecb30a77661fd536a36235f3a918103b2c36
tqlong1609 <tqlong1609@gmail.com>
2024-11-28 21:53:23 +0700

    add dist-adminlte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,34 @@
-import type { Metadata } from 'next'
-import { Noto_Sans_JP } from 'next/font/google'
-
-import '/public/plugins/fontawesome-free/css/all.min.css'
-import '/public/dist/css/adminlte.min.css'
-import '../scss/style.scss'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 // import '@/assets/style.scss'
 import BootstrapClient from '@/components/BootstrapClient'
 // import './globals.css';
 import ReactQueryProvider from '@/providers/ReactQueryProvider'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import type { Metadata } from 'next'
+import { Noto_Sans_JP } from 'next/font/google'
+
+import '../scss/style.scss'
+import '/public/dist-adminlte/css/adminlte.min.css'
+import '/public/plugins/fontawesome-free/css/all.min.css'
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ['latin'],
   weight: ['400', '500', '700'],
-  variable: '--font-noto-sans-jp',
+  variable: '--font-noto-sans-jp'
 })
 
 export const metadata: Metadata = {
   title: 'My Body Dashboard',
-  description: 'My Body Dashboard',
+  description: 'My Body Dashboard'
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+        <link
+          rel='stylesheet'
+          href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'
+        />
       </head>
       <body className={notoSansJP.className}>
         <ReactQueryProvider>
