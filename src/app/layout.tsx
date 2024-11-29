e87caedd089a7da90648352639303cdf1aaf82b1
tqlong1609 <tqlong1609@gmail.com>
2024-11-29 09:26:09 +0700

    feat: add aws-amplify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import BootstrapClient from '@/components/BootstrapClient'
 // import './globals.css';
 import ReactQueryProvider from '@/providers/ReactQueryProvider'
+import { Amplify } from 'aws-amplify'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import type { Metadata } from 'next'
 import { Noto_Sans_JP } from 'next/font/google'
@@ -9,6 +10,53 @@ import { Noto_Sans_JP } from 'next/font/google'
 import '../scss/style.scss'
 import '/public/dist-adminlte/css/adminlte.min.css'
 import '/public/plugins/fontawesome-free/css/all.min.css'
+
+/*
+      - MBD_NODE_ENV=development
+      - API_URL=https://development.hbs.symbol-develop.xyz
+      - COGNITO_USER_POOL_ID=ap-northeast-1_KXEQDxgjd
+      - COGNITO_USER_POOL_WEB_CLIENT_ID=2gmu1i5mvhujopsc0k420ba8h5
+      - COGNITO_REDIRECT_SIGNIN=http://localhost:3000/member/dashboard/
+      - COGNITO_REDIRECT_SIGNOUT=http://localhost:3000/member/dashboard/
+*/
+const runtimeConfig = {
+  MBD_NODE_ENV: 'development',
+  API_URL: 'https://development.hbs.symbol-develop.xyz',
+  COGNITO_USER_POOL_ID: 'ap-northeast-1_KXEQDxgjd',
+  COGNITO_USER_POOL_WEB_CLIENT_ID: '2gmu1i5mvhujopsc0k420ba8h5',
+  COGNITO_REDIRECT_SIGNIN: 'http://localhost:3001/member/dashboard/',
+  COGNITO_REDIRECT_SIGNOUT: 'http://localhost:3001/member/dashboard/'
+}
+
+const config = {
+  Auth: {
+    // identityPoolId: 'ap-northeast-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', //REQUIRED - Amazon Cognito Identity Pool ID
+    region: 'ap-northeast-1', // REQUIRED - Amazon Cognito Region
+    userPoolId: runtimeConfig.COGNITO_USER_POOL_ID, // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: runtimeConfig.COGNITO_USER_POOL_WEB_CLIENT_ID, // OPTIONAL - Amazon Cognito Web Client ID
+    oauth: {
+      scope: ['email', 'openid', 'aws.cognito.signin.user.admin'],
+      redirectSignIn: runtimeConfig.COGNITO_REDIRECT_SIGNIN, // Cognito に同じ設定が必要
+      redirectSignOut: runtimeConfig.COGNITO_REDIRECT_SIGNOUT, // Cognito に同じ設定が必要
+      responseType: 'code'
+    }
+  }
+}
+Amplify.configure({
+  auth: {
+    aws_region: 'ap-northeast-1',
+    user_pool_id: runtimeConfig.COGNITO_USER_POOL_ID,
+    user_pool_client_id: runtimeConfig.COGNITO_USER_POOL_WEB_CLIENT_ID,
+    oauth: {
+      scopes: ['email', 'openid', 'aws.cognito.signin.user.admin'],
+      redirect_sign_in_uri: [runtimeConfig.COGNITO_REDIRECT_SIGNIN],
+      redirect_sign_out_uri: [runtimeConfig.COGNITO_REDIRECT_SIGNOUT],
+      response_type: 'code',
+      identity_providers: [],
+      domain: 'localhost'
+    }
+  }
+})
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ['latin'],
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,14 +2,24 @@
 
 import React, { useState } from 'react'
 
+import { signIn } from 'aws-amplify/auth'
+
 const TestPage: React.FC = () => {
   const [userId, setUserId] = useState('')
   const [password, setPassword] = useState('')
   const [errors, setErrors] = useState<{ userId?: string; password?: string }>({})
 
-  const onSubmit = (event: React.FormEvent) => {
+  const onSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
-    // Handle form submission
+    console.log('userId', userId)
+    console.log('password', password)
+    signIn({ username: userId, password })
+      .then((result) => {
+        console.log('result', result)
+      })
+      .catch((error) => {
+        console.error('Error signing in:', error)
+      })
   }
 
   const userIdHandleChange = () => {
@@ -21,40 +31,40 @@ const TestPage: React.FC = () => {
   }
 
   return (
-    <div className="hold-transition login-page">
-      <div className="login-box">
-        <div className="login-logo">ログイン</div>
-        <div className="card">
-          <div className="card-body login-card-body">
-            <p className="login-box-msg">ユーザーIDとパスワードを入力してください</p>
+    <div className='hold-transition login-page'>
+      <div className='login-box'>
+        <div className='login-logo'>ログイン</div>
+        <div className='card'>
+          <div className='card-body login-card-body'>
+            <p className='login-box-msg'>ユーザーIDとパスワードを入力してください</p>
             <form onSubmit={onSubmit}>
-              <div className="form-group">
-                <label htmlFor="userId">ユーザーID</label>
+              <div className='form-group'>
+                <label htmlFor='userId'>ユーザーID</label>
                 <input
-                  id="userId"
+                  id='userId'
                   value={userId}
                   onChange={(e) => setUserId(e.target.value)}
-                  type="text"
+                  type='text'
                   className={`form-control ${errors.userId ? 'is-invalid' : ''}`}
                   onBlur={userIdHandleChange}
                 />
-                <div className="invalid-feedback">{errors.userId}</div>
+                <div className='invalid-feedback'>{errors.userId}</div>
               </div>
-              <div className="form-group">
-                <label htmlFor="password">パスワード</label>
+              <div className='form-group'>
+                <label htmlFor='password'>パスワード</label>
                 <input
-                  id="password"
+                  id='password'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  type="password"
+                  type='password'
                   className={`form-control ${errors.password ? 'is-invalid' : ''}`}
                   onBlur={passwordHandleChange}
                 />
-                <div className="invalid-feedback">{errors.password}</div>
+                <div className='invalid-feedback'>{errors.password}</div>
               </div>
-              <div className="row">
-                <div className="col-12">
-                  <button type="submit" className="btn btn-primary btn-block">
+              <div className='row'>
+                <div className='col-12'>
+                  <button type='submit' className='btn btn-primary btn-block'>
                     ログイン
                   </button>
                 </div>
